b63d005a50543f57a1b3befbb8d0c9b84fce3f17
akif-mehmood <muhammadakif148@gmail.com>
2025-08-07 16:30:50 +0500

    this is first code push

---
+++ app.js
@@ -0,0 +1,50 @@
+// var username
+// var myName ="Ahmed Raza"
+// var message
+// message ="Hello World"
+// alert(message)
+// var studentName ="Jameel"
+// var studentAge ="16"
+// var studentPassion ="Designer"
+// alert(studentName)
+// alert(studentAge)
+// alert(studentPassion)
+// var pizza ="PIZZA\nPIZZ\nPIZ\nPI\nP "
+// alert(pizza)
+// var email ="myemail@email.com"
+// alert(`My email address is ${email}`)
+// var book ="A smarter way to learn JavaScript"
+// alert(`I am trying to learn from the Book ${book}`)
+// document.writeln("Yah! I can write HTML content through JavaScript")
+
+var username
+username = "Akif Mehmood"
+var message
+message = "Hello I'm Akif Mehmood"
+
+alert(username)
+alert(message)
+var studentName = "Shazaib"
+var studentAge = "25"
+var studentPassion = "Developer"
+
+alert(studentName)
+alert(studentAge)
+alert(studentPassion)
+
+var pizza = "PIZZA\nPIZZ\nPIZ\nPI\nP"
+alert(pizza)
+
+var email = "muhammadakif148@gmail.com"
+alert(`My Email Address ${email}`)
+
+var book = "A smarter way to learn JavaScript"
+alert(`I am trying to learn from the Book ${book}`)
+
+document.writeln("Yaeh! I can write HTML content through JavaScript.")
+
+alert("“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬” ")
+
+
+
+
